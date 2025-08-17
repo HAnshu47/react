abb7d0030839fcbfbc88810df40583b161e02cdd
Huangrui47 <1477315575@qq.com>
2025-08-17 18:57:42 +0800

    配置vite

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import App from './App'
-// 导入store 模块
-import { Provider } from 'react-redux'
-import store from './store'
-
-const root = createRoot(document.getElementById('root'))
-root.render(
-  <Provider store={store}>
-  <App />
-  </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
