138dc448a52de362d1df857675f2a5e096551ec6
Huangrui47 <1477315575@qq.com>
2025-08-18 21:16:26 +0800

    tabBar高亮优化

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,4 +1,4 @@
-import { Outlet, } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import React from 'react';
 import { TabBar } from 'antd-mobile';
 import {
@@ -6,28 +6,31 @@ import {
   UnorderedListOutline,
   UserOutline
 } from 'antd-mobile-icons';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../../App.scss';
 
 export default function Index() {
+  // 获取当前页面路径，制作高亮
+  const location = useLocation();
   const tabs = [
     {
       key: '',
       title: '月账单',
-      icon: <AppOutline />,
+      icon: <AppOutline />
     },
     {
       key: 'record',
       title: '新增',
-      icon: <UnorderedListOutline />,
+      icon: <UnorderedListOutline />
     },
     {
       key: 'years',
       title: '年账单',
-      icon: <UserOutline />,
+      icon: <UserOutline />
     }
   ];
-  const  navigate = useNavigate();
+
+  const navigate = useNavigate();
   const setActiveKey = (key) => {
     navigate(`/${key}`);
   };
@@ -37,7 +40,10 @@ export default function Index() {
         <Outlet />
       </div>
       <div className="bottom">
-        <TabBar onChange={(key) => setActiveKey(key)}>
+        <TabBar
+          onChange={(key) => setActiveKey(key)}
+          activeKey={location.pathname.slice(1)}
+        >
           {tabs.map((item) => (
             <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
           ))}
