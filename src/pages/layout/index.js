ba2e7b2eb6a7c945d6b8ec18ad5947ebe7049ca2
Huangrui47 <1477315575@qq.com>
2025-08-20 10:06:01 +0800

    初始化极客园项目

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,53 +1,13 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import React from 'react';
-import { TabBar } from 'antd-mobile';
-import {
-  AppOutline,
-  UnorderedListOutline,
-  UserOutline
-} from 'antd-mobile-icons';
-import { useNavigate } from 'react-router-dom';
+
 import '../../App.scss';
 
 export default function Index() {
-  // 获取当前页面路径，制作高亮
-  const location = useLocation();
-  const tabs = [
-    {
-      key: '',
-      title: '月账单',
-      icon: <AppOutline />
-    },
-    {
-      key: 'record',
-      title: '新增',
-      icon: <UnorderedListOutline />
-    },
-    {
-      key: 'years',
-      title: '年账单',
-      icon: <UserOutline />
-    }
-  ];
-
-  const navigate = useNavigate();
-  const setActiveKey = (key) => {
-    navigate(`/${key}`);
-  };
   return (
     <div className="app">
       <div className="body">
         <Outlet />
-      </div>
-      <div className="bottom">
-        <TabBar
-          onChange={(key) => setActiveKey(key)}
-          activeKey={location.pathname.slice(1)}
-        >
-          {tabs.map((item) => (
-            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
-          ))}
-        </TabBar>
       </div>
     </div>
   );
--- src/store/modules/billLIst.js
+++ src/store/modules/billLIst.js
@@ -1,36 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getYearListColums, getAllMonthListColumn } from '../../utils/format';
 import http from '../../request/axios';
 
 const billListSlice = createSlice({
   name: 'billList',
   initialState: {
-    billList: [],
-    columnLists: []
   },
   reducers: {
-    setBillList(state, action) {
-      state.billList = action.payload;
-    },
-    setColumnLists(state, action) {
-      state.columnLists = getYearListColums(action.payload);
-    },
-    changeColumnLists(state, action) {
-      state.columnLists[1] = getAllMonthListColumn(action.payload);
-    }
+    // setBillList(state, action) {
+    //   state.billList = action.payload;
+    // },
   }
 });
 
 // 封装获取菜单的异步函数
-const { setBillList, setColumnLists, changeColumnLists } =
-  billListSlice.actions;
-const getBillList = () => async (dispatch) => {
-  const res = await http.get('/ka');
-  dispatch(setBillList(res));
-};
-const addBill = (payload) => async (dispatch) => {
-  await http.post('/ka', payload);
-};
-export { setBillList, getBillList, setColumnLists, changeColumnLists, addBill };
+const { setBillList } = billListSlice.actions;
+// const getBillList = () => async (dispatch) => {
+//   const res = await http.get('/ka');
+//   dispatch(setBillList(res));
+// };
+// const addBill = (payload) => async (dispatch) => {
+//   await http.post('/ka', payload);
+// };
+export { setBillList };
 const reducer = billListSlice.reducer;
 export default reducer;
